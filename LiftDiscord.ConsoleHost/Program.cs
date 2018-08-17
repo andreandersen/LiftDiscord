b9fdc87e94bccf47991afbad934414a4cdd72076
Andre Andersen <thezensei@gmail.com>
2018-08-18 01:41:17 +0200

    add config, upgrade to 2.1

--- LiftDiscord.ConsoleHost/Program.cs
+++ LiftDiscord.ConsoleHost/Program.cs
@@ -7,7 +7,7 @@ namespace LiftDiscord.ConsoleHost
     {
         public static async Task Main(string[] args)
         {
-            var client = new Client(".....");
+            var client = new Client("NDExNTIzNDcxODQ4Mzc0Mjcy.DkigUw.0p-Z1ym3keA47jD8ePx-GxwJaj4");
             await client.RunAsync();
         }
     }
