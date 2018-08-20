618d21d31aacbc42c178638465db2b9c34d0ac20
Andre Andersen <thezensei@gmail.com>
2018-08-20 03:39:48 +0200

    testing ci-cd pipeline

--- LiftDiscord.DiscordClient/Commands/LiftingBuildsModule.cs
+++ LiftDiscord.DiscordClient/Commands/LiftingBuildsModule.cs
@@ -9,6 +9,25 @@ namespace LiftDiscord.DiscordClient.Commands
 {
     public class LiftingBuildsModule : ModuleBase
     {
+        [Command("Frostbolt2")]
+        public async Task liftingFrostbolt2()
+        {
+            //Creates the URL variable that links to  build, needs to be updated if builds move
+            var embed = new EmbedBuilder()
+                  .WithColor(Color.Blue)
+                  .WithTitle("Liftings frostbolt Totem Hierophant 3.3 Guide")
+                  .WithThumbnailUrl("https://i.imgur.com/RFbq7b2.png")
+                  .WithUrl("https://www.pathofexile.com/forum/view-thread/1931084")
+                  .WithDescription("Liftings Frostbolt Hierophant is here to serve up some nice chilly death on a budget")
+                  .WithFooter("https://www.pathofexile.com/forum/view-thread/1931084")
+                  .Build();
+
+            //Reply from the bot with the embeded URL as an argument
+            await ReplyAsync(
+            "Liftings frostbolt totem build is still viable! <:lnbFist:480141292417056768> " + "\n" +
+               "Here is the link to the forum page that is " + "updated every new league!", false, embed, null);
+        }
+
         //Frostbolt command
         [Command("Frostbolt")]
         public async Task liftingFrostbolt()
